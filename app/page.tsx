93a7c52eced0ac7bde7b476076d74c4405ba5de1
navyan6 <110777857+navyan6@users.noreply.github.com>
2025-05-16 16:50:09 -0400

    test page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Currently a student at Georgia Tech where I build software and eat data. 
           </li>
         </ol>
 
