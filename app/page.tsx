c3b7b44464daf0d8e544d397e81cf8fbffc04c4e
navyan6 <navyanori6@gmail.com>
2025-05-23 15:46:54 -0400

    fix_main_page

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,11 @@ export default function Home() {
         <h1 className="text-6xl font-extrabold text-white mt-8">navya nori</h1>
         <p className="text-xl text-white mt-4">
           atlanta // georgia tech <br />
-          i'm a cs student at georgia tech interested in machine learning, sustainable AI, reading, & running. <br />
-          come along with me as i learn about the world and write about the anthropocene!
+          i'm a cs student at georgia tech interested in machine learning, sustainable AI, reading, & running <br />
+          <span className="text-lg text-gray-300 animate-fill-text">
+            "those who can imagine anything can create the impossible" - alan turing
+          </span>
         </p>
-      </div>
-
-      {/* Alan Turing Quote */}
-      <div className="text-lg text-gray-300 mt-6 animate-fill-text">
-        "those who can imagine anything can create the impossible" - alan turing
       </div>
     </div>
   );
