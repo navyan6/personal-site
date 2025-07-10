3f02512f438ae12690e7a9f3dad40603529abbfc
navya <navyanori6@gmail.com>
2025-07-10 11:28:24 -0400

    ui_change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center relative min-h-screen flex flex-col justify-between">
       <div>
-        <h1 className="text-6xl font-extrabold text-white mt-8">Navya Nori</h1>
+        <h1 className="text-2xl font-extrabold text-white mt-8">Navya Nori</h1>
         <p className="text-xl text-white mt-4">
           Atlanta // Georgia Tech <br />
           I'm a CS student at Georgia Tech, broadly interested in machine learning, interpretable & safe AI, and AI for molecular discovery. <br />
